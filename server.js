ea95b733c5d5909822ad0da33f0aa9d4b5bc86b6
Diogo <diogoess123@gmail.com>
2023-06-05 15:45:08 -0300

    feat: create api register user

---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+
+
+
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "crud"
+    }
+)
+
+app.get("/",(req,res) =>{
+    const sql = "SELECT * FROM estoque";
+    db.query(sql,(err,data)=>{
+        if(err) return res.json("Erro");
+        return res.json(data);
+    })
+})
+
+app.post('/create',(req,res)=>{
+
+    const sql = "INSERT INTO estoque (`nome`,`modelo`,`patrimonio`,`entrada`,`saida`,`local`) VALUES (?)";
+    const values = [
+        req.body.nome,
+        req.body.modelo,
+        req.body.patrimonio,
+        req.body.entrada,
+        req.body.saida,
+        req.body.local
+
+    ]
+    db.query(sql, [values], (err,data)=>{
+        if(err) return res.json("Error");
+        return res.json(data);
+        
+    })
+})
+
+app.put('/update/:id',(req,res)=>{
+
+    const sql = "update estoque set `nome` = ?, `modelo` =?, `patrimonio` = ?, `entrada` = ?, `saida` = ?, `local` = ? where id = ?";
+    const values = [
+        req.body.nome,
+        req.body.modelo,
+        req.body.patrimonio,
+        req.body.entrada,
+        req.body.saida,
+        req.body.local
+
+    ]
+    const id =req.params.id;
+    db.query(sql, [...values, id], (err,data)=>{
+        if(err) return res.json("Error");
+        return res.json(data);
+        
+    })
+})
+
+
+app.delete('/estoque/:id',(req,res)=>{
+
+    const sql = "DELETE FROM estoque WHERE ID = ?";
+    
+    const id =req.params.id;
+    db.query(sql, [id], (err,data)=>{
+        if(err) return res.json("Error");
+        return res.json(data);
+        
+    })
+})
+
+
+
+
+
+app.listen(8081, () =>{
+    console.log("listening");
+})
+
+
+
